2de8ed642d5dc6ff209577a203709422930acf67
Malika <malikauku@gmail.com>
2021-03-14 15:22:59 +0700

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length > 0) {
+  		return Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length > 0) {
+  		return Math.max(...array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length > 0) {
+	  let total = 0;
+	  for (let i = 0; i < array.length; i++) {
+	   total += array[i];
+	  }
+	  return Math.floor((total / array.length) * 100) / 100;
+  }
   return 0;
 }
